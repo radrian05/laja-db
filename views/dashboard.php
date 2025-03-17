f6ac7e59a31ad80653e1c2ccfb00e506b3555300
Adrian Rojas <radriandavid@gmail.com>
2025-03-17 17:10:44 -0400

    renombrar

--- views/dashboard.php
+++ views/dashboard.php
@@ -12,7 +12,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Productos</title>
-    <link rel="stylesheet" href="sidebar.css" type="text/css">
+    <link rel="stylesheet" href="dashboard.css" type="text/css">
 </head>
 
 <?php generateSidebar(basename($_SERVER['PHP_SELF'])); ?>
